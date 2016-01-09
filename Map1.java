8a985d5fd0c8f405e7e411909aee83d586082fbb
Yiruz <2yi.z.ru2@gmail.com>
2016-01-08 22:14:43 -0500

    Created Map1.java and drew basic background

---
+++ Map1.java
@@ -0,0 +1,15 @@
+public class Map1{
+    public void setup(){
+	size(500, 400);
+	noStroke();
+    }
+
+    public void draw(){
+	background(65,188,55);
+	rect(200,50,280,300);
+	rect(450,150,50,100);
+	ellipse(200,200,380,300);
+	rect(200,350,100,50);
+	fill(255,222,173);
+    }
+}
